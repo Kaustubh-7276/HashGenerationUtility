7a63cc37642f010e69b291ca2fc6cddc0772133a
itskaustubhtodmal <kaustubhtodmal@gmail.com>
2026-04-01 15:08:06 +0530

    Create HashEntry.cs

---
+++ HashEntry.cs
@@ -0,0 +1,8 @@
+namespace BancsHashGenrator.ViewModels
+{
+    public class HashEntry
+    {
+        public string RawText { get; set; } = string.Empty;
+        public string HashResult { get; set; } = string.Empty;
+    }
+}
